453b79fa0fa5b7686f0582abc06967796be6d541
rand0me <not.randome@gmail.com>
2018-07-06 15:56:07 +0300

    docs: Fix docs path #2

--- tools/gh-pages-publish.js
+++ tools/gh-pages-publish.js
@@ -3,8 +3,7 @@ const ghpages = require('gh-pages');
 /**
  * This configuration will avoid logging the GH_TOKEN if there is an error.
  */
-ghpages.publish('docs', {
-
+ghpages.publish('../docs', {
     repo: 'https://' + process.env.GH_TOKEN + '@github.com/rand0me/redux-appinsights-middleware.git',
     silent: true
 }, (err) => err ? console.error(err) : console.log("Done."));
