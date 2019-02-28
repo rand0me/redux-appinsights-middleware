d840183dbaff78e16570c3a5b0b484ea5df5fdce
rand0me <not.randome@gmail.com>
2019-02-28 14:10:40 +0300

    feat: Make middleware to read a property from action.meta
    BREAKING CHANGE: Middleware no longer reads track info from action[prop] but instead from
    action.meta[prop]

--- src/redux-appinsights.ts
+++ src/redux-appinsights.ts
@@ -1,15 +1,21 @@
-import 'applicationinsights-js'
+import { AppInsights } from 'applicationinsights-js'
 import { Dispatch, Middleware, AnyAction } from 'redux'
 
 type InsightsEvent = {
-  method: keyof Microsoft.ApplicationInsights.IAppInsights
-  data: any
+  method: string
+  data: any[]
 }
 
 const SETUP = 'appinsights/SETUP'
 
-export function setup(key: string) {
-  return { type: SETUP, payload: key }
+type SetupAction = { type: string; payload: typeof AppInsights.config }
+
+function isSetupAction(action: AnyAction): action is SetupAction {
+  return action.type === SETUP
+}
+
+export function setup(config: typeof AppInsights.config): SetupAction {
+  return { type: SETUP, payload: config }
 }
 
 /**
@@ -22,14 +28,15 @@ export function createAppInsightsMiddleware(
   propName = 'appinsights'
 ): Middleware {
   return ({ dispatch, getState }) => (next: Dispatch<AnyAction>) => (action: AnyAction) => {
-    if (action.type === SETUP && ai.downloadAndSetup) {
-      ai.downloadAndSetup({ instrumentationKey: action.payload })
+    if (isSetupAction(action) && ai.downloadAndSetup) {
+      ai.downloadAndSetup(action.payload)
+    } else {
+      const params: InsightsEvent = action.meta && action.meta[propName]
+      if (params) {
+        ;(ai as any)[params.method](...params.data)
+      }
+      next(action)
     }
-    const params: InsightsEvent = action[propName]
-    if (params) {
-      ;(ai as any)[params.method](...params.data)
-    }
-    next(action)
   }
 }
 
@@ -39,8 +46,8 @@ export function createAppInsightsMiddleware(
  */
 export function createAppInsightsReducer(propName = 'appinsights') {
   return (state = { events: [] as InsightsEvent[] }, action: AnyAction) => {
-    if (action[propName]) {
-      return { events: state.events.concat(action[propName]) }
+    if (action.meta && action.meta[propName]) {
+      return { events: state.events.concat(action.meta[propName]) }
     } else {
       return state
     }
